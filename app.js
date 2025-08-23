382c167c0b9cd88551b0e8552fcec1fdec2a5ff5
Mohit verma <mohitverma98157899@example.com>
2025-08-23 14:56:50 +0530

    bracket porject express

---
+++ app.js
@@ -0,0 +1,36 @@
+const express= require('express');
+const path= require('path');
+const app= express();
+// env file
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Pouter file
+const siteRouter= require('./router/site.routes');
+
+// Public folder 
+app.use(express.static(path.join(__dirname,'public')));
+
+// EJS template view engine
+app.set('view engine','ejs');
+
+// view folder dirname
+app.set('views',path.join(__dirname,'views'));
+
+// Router 
+app.use('/',siteRouter);
+
+// Error handel
+app.use((req,res)=>{
+    res.status(404).render('pages/notfound')
+});
+
+// Use env PORT 
+const PORT = process.env.PORT;
+
+
+
+// server
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`)
+});
---
+++ controllers/site.controller.js
@@ -0,0 +1,67 @@
+// pages folder files
+function renderFile(res,pageName,title){
+    return res.render(`pages/${pageName}`,{title:title})
+}
+
+// pages folder files
+exports.home=(req,res)=>renderFile(res,'index','Bracket Responsive Bootstrap3 Admin');
+exports.alerts=(req,res)=>renderFile(res,'alerts','alerts');
+exports.blank=(req,res)=>renderFile(res,'blank','blank');
+exports.blog_list=(req,res)=>renderFile(res,'blog-list','blog-list');
+exports.blog_single=(req,res)=>renderFile(res,'blog-single','blog-single');
+exports.bug_issues=(req,res)=>renderFile(res,'bug-issues','bug-issues');
+exports.bug_tracker=(req,res)=>renderFile(res,'bug-tracker','bug-tracker');
+exports.buttons=(req,res)=>renderFile(res,'buttons','buttons');
+exports.calendar=(req,res)=>renderFile(res,'calendar','calendar');
+exports.code_editor=(req,res)=>renderFile(res,'code-editor','code-editor');
+exports.compose=(req,res)=>renderFile(res,'compose','compose');
+exports.email=(req,res)=>renderFile(res,'email','email');
+exports.extras=(req,res)=>renderFile(res,'extras','extras');
+exports.fixed_width_noleft=(req,res)=>renderFile(res,'fixed-width-noleft','fixed-width-noleft');
+exports.fixed_width=(req,res)=>renderFile(res,'fixed-width','fixed-width');
+exports.fixed_width2=(req,res)=>renderFile(res,'fixed-width2','fixed-width2');
+exports.form_layouts=(req,res)=>renderFile(res,'form-layouts','form-layouts');
+exports.form_validation=(req,res)=>renderFile(res,'form-validation','form-validation');
+exports.form_wizards=(req,res)=>renderFile(res,'form-wizards','form-wizards');
+exports.general_forms=(req,res)=>renderFile(res,'general-forms','general-forms');
+exports.graphs=(req,res)=>renderFile(res,'graphs','graphs');
+exports.horizontal_menu=(req,res)=>renderFile(res,'horizontal-menu','horizontal-menu');
+exports.horizontal_menu2=(req,res)=>renderFile(res,'horizontal-menu2','horizontal-menu2');
+exports.icons=(req,res)=>renderFile(res,'icons','icons');
+exports.invoice=(req,res)=>renderFile(res,'invoice','invoice');
+exports.layouts=(req,res)=>renderFile(res,'layouts','layouts');
+exports.locked=(req,res)=>renderFile(res,'locked','locked');
+exports.maps=(req,res)=>renderFile(res,'maps','maps');
+exports.media_manager=(req,res)=>renderFile(res,'media-manager','media-manager');
+exports.modals=(req,res)=>renderFile(res,'modals','modals');
+exports.notfound=(req,res)=>renderFile(res,'notfound','Notfound');
+exports.people_directory=(req,res)=>renderFile(res,'people-directory','people-directory');
+exports.profile=(req,res)=>renderFile(res,'profile','profile');
+exports.read=(req,res)=>renderFile(res,'read','read');
+exports.search_results=(req,res)=>renderFile(res,'search-results','search-results');
+exports.singin=(req,res)=>renderFile(res,'signin','signin');
+exports.signup=(req,res)=>renderFile(res,'signup','signup');
+exports.sliders=(req,res)=>renderFile(res,'sliders','sliders');
+exports.tables=(req,res)=>renderFile(res,'tables','tables');
+exports.tabs_accordions=(req,res)=>renderFile(res,'tabs-accordions','tabs-accordions');
+exports.timeline=(req,res)=>renderFile(res,'timeline','timeline');
+exports.typography=(req,res)=>renderFile(res,'typography','typography');
+exports.view_issue=(req,res)=>renderFile(res,'view-issue','view-issue');
+exports.widgets=(req,res)=>renderFile(res,'widgets','widgets');
+exports.wysiwyg=(req,res)=>renderFile(res,'wysiwyg','wysiwyg');
+exports.x_editable=(req,res)=>renderFile(res,'x-editable','x-editable');
+
+//ajax folder file
+
+function ajaxFolder(res,fileName){
+    return res.render(`ajax/${fileName}`)
+};
+
+exports.accordion=(req,res)=>ajaxFolder(res,'accordion')
+exports.panel=(req,res)=>ajaxFolder(res,'panel')
+exports.photo_viewer_rtl=(req,res)=>ajaxFolder(res,'photo-viewer-rtl')
+exports.photo_viewer=(req,res)=>ajaxFolder(res,'photo-viewer')
+exports.remote=(req,res)=>ajaxFolder(res,'remote')
+exports.tabs=(req,res)=>ajaxFolder(res,'tabs')
+
+
